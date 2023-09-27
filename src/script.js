8ce86d683b11745d8a32ce0f803be3ea99d9b0a2
Ielmarzouqy <elmarzouqyimane@gmail.com>
2023-09-27 08:48:37 +0100

    add function generate possibilities

--- src/script.js
+++ src/script.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     let board = document.getElementById('board');
     board.classList.remove('hide');
     let sidebar = document.getElementById('sidebar');
-    sidebar.classList.remove('hide');
+    // sidebar.classList.remove('hide');
   }
 
 function setIconx(cell){
@@ -62,55 +62,140 @@ function score() {
 
 function checkWinnerAxeX(arr) {
   const possibility = [1, 2, 3, 4, 5];
-                                            
+                      
   console.log("checkWinner")
   for (let i = 0; i <= arr.length - 5 ; i++) {
     const subarray = arr.slice(i, i + 5);
     const differences = subarray.map((val, index) => val - subarray[0]);
-
-    if (possibility.includes(differences[1]) &&
-        differences.every((diff, index) => diff === differences[0] + index) 
-         ) {
+    
+    if ( possibility.includes(differences[1]) &&
+        differences.every((diff, index) => diff === differences[0] + index ) 
         
+        ) {
       return subarray;
     }
   }
-  
   return [];
 }
 
 function handleClick(event, cell) {
   if (!cell.hasChildNodes()) {
-  
+    let move = parseInt(cell.getAttribute('data-value'));
     if (currentPlayer === "X") {
-      lastXMoves.push(parseInt(cell.getAttribute('data-value')));
-      lastXMoves.sort(function(a,b){return a-b});
-      const result = checkWinnerAxeX(lastXMoves, currentPlayer );
+      // lastXMoves.push(parseInt(cell.getAttribute('data-value')));
+      // lastXMoves.sort(function(a,b){return a-b});
+      // const result = checkWinnerAxeX(lastXMoves, currentPlayer );
 
       setIconx(cell);
       currentPlayer = "O";
-      console.log(lastXMoves)
+      // console.log(lastXMoves)
 
-      if (result.length > 0) {
-        console.log("Winner is the player X", result);
-      } else {
-        console.log(lastXMoves);
+      // if (result.length > 0) {
+      //   console.log("Winner is the player X", result);
+      // } else {
+      //   console.log("x",lastXMoves);
+      
+      // }
+
+      if(checkIfWin(move,lastXMoves)){
+        console.log("x wins");
       }
     } else {
       
-      lastOMoves.push(parseInt(cell.getAttribute('data-value')));
-      lastOMoves.sort(function(a,b){return a-b});
-      const result = checkWinnerAxeX(lastOMoves, currentPlayer );
+      // lastOMoves.push(parseInt(cell.getAttribute('data-value')));
+      // lastOMoves.sort(function(a,b){return a-b});
+      // const result = checkWinnerAxeX(lastOMoves, currentPlayer );
 
       setIcono(cell);
       currentPlayer = "X";
-      console.log(lastOMoves)
-
-      if (result.length > 0) {
-        console.log("Winner is the player O", result);
-      } else {
-        console.log(lastOMoves);
+      if(checkIfWin(move,lastOMoves)){
+        console.log("O wins");
       }
+      // console.log(lastOMoves)
+
+      // if (result.length > 0) {
+      //   console.log("Winner is the player O", result);
+      // } else {
+      //   console.log("OO",lastOMoves);
+      // }
     }
   }
 }
+
+
+function generatePossibilities(number ){
+    let possibilies = [];   
+    possibilies = getXPossibilities(possibilies,number);
+    possibilies = getYPossibilities(possibilies,number);
+
+
+  console.log(possibilies);
+    return possibilies;
+}
+
+function getXPossibilities(possibilies, number){
+  let helper = number%20;
+  if(number%20 > 4){
+    helper = 4
+  }
+  for(let j = helper; j >=helper - 4 ;j--){
+    let arr = [];
+    for(let i = j; i >= j-4; i--){
+      if(arr.length < 5 && number%20 - i >= 1 && number%20 - i <= 20){
+        arr.push(number - i);
+      }
+    }
+    if(arr.length == 5 && arr.includes(number)){
+      possibilies.push(arr);
+    }
+  }
+  return possibilies;
+}
+
+function getYPossibilities(possibilies, number){
+  let helper = Math.floor(number /20);
+  if(number /20 > 4){
+    helper = 4
+  }
+
+
+  for(let j = helper; j >=helper - 4 ;j--){
+    let arr = [];
+    for(let i = j; i >= j-4; i--){
+      if(arr.length < 5 && number - 20*i >= 1 && number - 20*i <= 400){
+        arr.push(number - 20*i);
+      }
+    }
+    if(arr.length == 5 && arr.includes(number)){
+      possibilies.push(arr);
+    }
+  }
+
+  return possibilies;
+}
+
+
+
+
+// generatePossibilities(22);
+
+function checkIfWin(number,moves){
+  moves.push(number);
+  let possibilies = generatePossibilities(number);
+  for(let i =0;i<possibilies.length ;i++){
+    if(
+      moves.includes(possibilies[i][0]) 
+      && moves.includes(possibilies[i][1])
+      && moves.includes(possibilies[i][2])
+      && moves.includes(possibilies[i][3])
+      && moves.includes(possibilies[i][4])
+
+    ){
+      return true;
+    }
+  }
+  return false;
+
+}
+
+// console.log(checkIfWin(22,[21,23,44,25,89,24,70,83,123]))
